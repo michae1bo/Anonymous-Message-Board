b288cbf5248c9f53ef0b6f6193593bad15e1dcac
michae1bo <159921466+michae1bo@users.noreply.github.com>
2025-03-04 13:45:09 +0000

    feat: add mongoose schemas

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,39 @@
 'use strict';
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 
 module.exports = function (app) {
+
+  const replySchema = new mongoose.Schema({
+    text: String,
+    created_on: {type: String, default: new Date().toLocaleString()},
+    delete_password: String,
+    reported: {type: Boolean, default: false}
+  })
+
+  const threadSchema = new mongoose.Schema({
+    text: String,
+    created_on: {type: String, default: new Date().toLocaleString()},
+    bumped_on: {type: String, default: new Date().toLocaleString()},
+    reported: {type: Boolean, default: false},
+    delete_password: String,
+    replies: [replySchema]
+  })
+
+  const boardSchema = new mongoose.Schema({
+    name: String,
+    threads: [threadSchema]
+  })
+
+  const BoardDB = mongoose.model('BoardDB', boardSchema);
+
+  mongoose.connect(process.env.MONGO_URI);
   
   app.route('/api/threads/:board');
     
   app.route('/api/replies/:board');
 
 };
+
+
