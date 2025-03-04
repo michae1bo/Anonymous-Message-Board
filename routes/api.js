acde109113ef80c48eb4df1e404b7f1924957635
michae1bo <159921466+michae1bo@users.noreply.github.com>
2025-03-04 20:56:22 +0000

    feat: implement post and get replies api

--- routes/api.js
+++ routes/api.js
@@ -7,15 +7,15 @@ module.exports = function (app) {
 
   const replySchema = new mongoose.Schema({
     text: String,
-    created_on: {type: String, default: new Date().toLocaleString()},
+    created_on: {type: Date, default: new Date()},
     delete_password: String,
     reported: {type: Boolean, default: false}
   })
 
   const threadSchema = new mongoose.Schema({
     text: String,
-    created_on: {type: String, default: new Date().toLocaleString()},
-    bumped_on: {type: String, default: new Date().toLocaleString()},
+    created_on: {type: Date, default: new Date()},
+    bumped_on: {type: Date, default: new Date()},
     reported: {type: Boolean, default: false},
     delete_password: String,
     replies: {type: [replySchema], default: []}
@@ -36,7 +36,7 @@ module.exports = function (app) {
       const boardData = await BoardDB.findOne({name: boardName});
       const returnArray = [];
       if (boardData !== null) {
-        const threads = boardData.threads.sort((a, b) => new Date(b.bumped_on) - new Date(a.bumped_on))
+        const threads = boardData.threads.sort((a, b) => b.bumped_on - a.bumped_on)
         const latest_threads = threads.slice(0, 10)
         latest_threads.forEach((thread => {
           const returnThread = {
@@ -46,10 +46,10 @@ module.exports = function (app) {
             _id: thread._id.toString(),
             replies: []
           };
-          const replies = thread.replies.sort((a, b) => new Date(b.created_on) - new Date(a.created_on));
+          const replies = thread.replies.sort((a, b) => b.created_on - a.created_on);
           const latest_replies = replies.slice(0, 3);
           latest_replies.forEach(reply => {
-            returnThread.push({text: reply.text, created_on: reply.created_on, _id: reply._id.toString()})
+            returnThread.replies.push({text: reply.text, created_on: reply.created_on, _id: reply._id.toString()})
           })
           returnArray.push(returnThread);
         }))
@@ -82,7 +82,7 @@ module.exports = function (app) {
           }
         }
         if (!foundThread) {
-          returnString = 'No such thread in this board'
+          returnString = 'No such thread on this board'
         }
       }
       res.send(returnString);
@@ -94,13 +94,50 @@ module.exports = function (app) {
     });
     
   app.route('/api/replies/:board')
-  .get()
+  .get(async function (req, res){
+    let boardData = await BoardDB.findOne({name: req.params.board.toLowerCase()});
+    let returnObject;
+    if (boardData !== null) {
+      console.log(req.params);
+      console.log(req.query);
+      for (let i = 0; i < boardData.threads.length; i++) {
+        if (boardData.threads[i]._id.toString() === req.query.thread_id) {
+          const thread = boardData.threads[i];
+          returnObject = {
+            text: thread.text,
+            created_on: thread.created_on,
+            bumped_on: thread.bumped_on,
+            _id: thread._id.toString(),
+            replies: []
+          };
+          thread.replies.forEach(reply => {
+            returnObject.replies.push({text: reply.text, created_on: reply.created_on, _id: reply._id.toString()})
+          })
+        }
+      }
+    } else {
+      returnObject = {}
+    }
+    res.json(returnObject);
+  })
   .post(async function (req, res) {
-
+    let returnObject;
     let boardData = await BoardDB.findOne({name: req.params.board.toLowerCase()});
     if (boardData !== null) {
-
+      for (let i = 0; i < boardData.threads.length; i++) {
+        if (boardData.threads[i]._id.toString() === req.body.thread_id) {
+          const currentTime = new Date();
+          boardData.threads[i].replies.push({text: req.body.text, delete_password: req.body.delete_password, created_on: currentTime});
+          boardData.threads[i].bumped_on = currentTime;
+          const newBoard = await boardData.save();
+          returnObject = newBoard.threads[i].replies.at(-1);
+          break;
+        }
+      }
+    } else {
+      returnObject = {};
     }
+    res.json(returnObject);
   })
   .put()
   .delete();
